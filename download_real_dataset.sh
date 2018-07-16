ae8dd910120283ddc530c2de8772c82ee1e8dc18
Borrego <joaohenriqueborrego@gmail.com>
2018-07-16 15:03:56 +0100

    fix dataset url

--- download_real_dataset.sh
+++ download_real_dataset.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/bash
 
-#url="http://soma.isr.ist.utl.pt/vislab_data/shape_detection/shape_detection.tar.gz" 
-url="http://web.tecnico.ulisboa.pt/~joao.borrego/new_SHAPES_2018.tar.gz" 
+url="http://soma.isr.ist.utl.pt/vislab_data/shapes2018/shapes2018.tar.gz" 
 
 # Donwload
-wget ${url} -q -O shape_detection.tar.gz
+wget ${url} -q -O shapes2018.tar.gz
 # Unpack
 mkdir -p dataset/
-tar xzf shape_detection.tar.gz -C dataset/
+tar xzf shapes2018.tar.gz -C dataset/
 # Cleanup
 mkdir -p workspace/data 
 cp dataset/processed/* workspace/data/
 cp workspace/data/train.record workspace/data/real.record
-rm -rf shape_detection.tar.gz
+rm -rf shapes2018.tar.gz
 
 echo "Sucessfully downloaded dataset!"
